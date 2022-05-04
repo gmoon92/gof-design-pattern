96ef8fde02156bbfb559e6b592baacbb485e4ed6
gmoon92 <gmoon0929@gmail.com>
2022-05-05 04:40:16 +0900

    refactor: Factory method 패턴 클라이언트 코드 수정

--- design-patterns/src/main/java/com/gmoon/designpatterns/creational/factorymethod/Client.java
+++ design-patterns/src/main/java/com/gmoon/designpatterns/creational/factorymethod/Client.java
@@ -3,11 +3,12 @@ package com.gmoon.designpatterns.creational.factorymethod;
 public class Client {
 
 	public static void main(String[] args) {
-		Ship whiteship = new WhiteShipFactory().orderShip("Whiteship", "keesun@mail.com");
-		System.out.println(whiteship);
-
-		Ship blackship = new BlackPearlFactory().orderShip("Blackship", "keesun@mail.com");
-		System.out.println(blackship);
+		Client client = new Client();
+		client.print(new WhiteShipFactory(), "Whiteship", "keesun@mail.com");
+		client.print(new BlackPearlFactory(), "Blackship", "keesun@mail.com");
 	}
 
+	private void print(ShipFactory factory, String name, String email) {
+		System.out.println(factory.orderShip(name, email));
+	}
 }
