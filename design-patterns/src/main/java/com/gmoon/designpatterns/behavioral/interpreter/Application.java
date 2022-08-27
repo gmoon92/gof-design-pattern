83909c744afbad698c962a6b8efea2ceee7b5f3f
gmoon92 <gmoon0929@gmail.com>
2022-08-28 01:34:24 +0900

    feat(Interpreter): 인터프리터 패턴 client 객체 추가

--- design-patterns/src/main/java/com/gmoon/designpatterns/behavioral/interpreter/Application.java
+++ design-patterns/src/main/java/com/gmoon/designpatterns/behavioral/interpreter/Application.java
@@ -1,13 +1,17 @@
 package com.gmoon.designpatterns.behavioral.interpreter;
 
-import com.gmoon.designpatterns.behavioral.interpreter.notation.PostfixNotation;
+import com.gmoon.designpatterns.behavioral.interpreter.expression.PostfixExpression;
 
 public class Application {
 
 	public static void main(String[] args) {
-		PostfixNotation postfixNotation = new PostfixNotation("123+-");
-		int sum = postfixNotation.calculate();
+		Context context = Context.create()
+		  .put('x', 1)
+		  .put('y', 2)
+		  .put('z', 3);
 
-		System.out.println(sum);
+		PostfixExpression expression = PostfixParser.parse("xyz+-");
+		int result = expression.interpret(context);
+		System.out.println(result);
 	}
 }
